cefb9c289ae31d4a0d002d27cc107d2cbf73f84f
Gabriel <gabriel.sgaspar@yahoo.com.br>
2020-02-08 11:21:34 -0300

    Criando componente SideMenu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 
+import SideMenu from './components/SideMenu'
+
 function App() {
   return (
     <div className="App">
@@ -8,17 +10,7 @@ function App() {
       <header>header</header>
       <footer>cabeçalho</footer>
       <nav>
-        <div className = "sideMenu">
-          <div className = "ImageSection">
-            <div className="item">
-
-            </div>
-          </div>
-          <div className="ContactSection">
-            navegação <br/>
-            navegação 
-          </div>
-        </div>
+          <SideMenu />
       </nav>
     </div>
   );
---
+++ src/components/SideMenu/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import './index.css'
+
+const sideMenu = props => {
+    return (
+        <div className = "sideMenu">
+            <div className = "imageSection">
+                <div className="item">
+
+                </div>
+            </div>
+            <div className="contactSection">
+                navegação <br/>
+                navegação 
+            </div>
+      </div>
+    )
+}
+
+export default sideMenu;
